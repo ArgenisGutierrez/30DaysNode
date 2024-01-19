06e73bb522500b2c898823a030820bdbcb2ff691
ArgenisGutierrez <argenis.v.gtz@gmail.com>
2024-01-19 14:46:53 -0600

    Day 6 Arrays
    Dia 6 creacion de arreglos y sus metodos

---
+++ Day-6-Arrays/array.js
@@ -0,0 +1,5 @@
+
+var names = ["rj", "ricky", "alex"];
+var arr = new Array(3);
+arr = [1, 5, 7];
+var arr1 = new Array(2, 5, 7); 
---
+++ Day-6-Arrays/splice.js
@@ -0,0 +1,11 @@
+//Agregar
+arr = ['b', 'e', 'a', 'o', 'p', 'n', 'r'];
+console.log(arr);
+arr.splice(2, 2, 'rj', 'nodejs');
+console.log(arr);
+
+//Eliminar
+arr = ['b', 'e', 'a', 'o', 'p', 'n', 'r'];
+console.log(arr);
+arr.splice(2, 2);
+console.log(arr);			
